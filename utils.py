7d957ecdd983db74fe76d93e291536a7db59ad16
Vladimir Chernykh <vladimir.chernykh@phystech.edu>
2020-04-07 18:22:19 +0300

    replace outdated load_lua method

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
-from torch.utils.serialization import load_lua
+from torchfile import load as load_lua
 from torch.utils.data import DataLoader
 from networks import Vgg16
 from torch.autograd import Variable
