2282d6de974605b9296992f42956e71416b114c3
Vladimir Chernykh <vladimir.chernykh@phystech.edu>
2020-05-18 23:29:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
 from __future__ import print_function
-from utils import get_config, pytorch03_to_pytorch04
-from trainer import MUNIT_Trainer, UNIT_Trainer
+from .utils import get_config, pytorch03_to_pytorch04
+from .trainer import MUNIT_Trainer, UNIT_Trainer
 import argparse
 from torch.autograd import Variable
 import torchvision.utils as vutils
