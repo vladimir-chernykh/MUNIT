600d41416cb9877918a29f8e0e9c68d7a177ef07
Vladimir Chernykh <vladimir.chernykh@phystech.edu>
2020-05-18 23:21:57 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="munit",
+    version="1.0",
+    description="MUNIT GAN",
+    author="Vladimir Chernykh",
+    author_email="vladimir.chernykh@phystech.edu",
+    packages=["munit"],
+    install_requires=[]
+)
