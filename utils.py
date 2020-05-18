74accf403cdd54bec30034ad35f1f32347cc3ed0
Vladimir Chernykh <vladimir.chernykh@phystech.edu>
2020-05-18 23:28:13 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,11 +4,11 @@ Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses
 """
 from torchfile import load as load_lua
 from torch.utils.data import DataLoader
-from networks import Vgg16
+from .networks import Vgg16
 from torch.autograd import Variable
 from torch.optim import lr_scheduler
 from torchvision import transforms
-from data import ImageFilelist, ImageFolder
+from .data import ImageFilelist, ImageFolder
 import torch
 import torch.nn as nn
 import os
